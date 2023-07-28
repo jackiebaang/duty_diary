7e1d83aa8841f962569f2b2201d9eb5b153cfb8d
Franz <fgelsano@gmail.com>
2023-07-29 06:26:28 +0800

    Setup the admin panel

---
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -0,0 +1,40 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-book fa-solid"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Duty Diary</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-solid fa-book-open"></i>
+            <span>Diaries</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-solid fa-camera-retro"></i>
+            <span>Documentations</span></a>
+    </li>
+
+</ul>
+<!-- End of Sidebar -->
