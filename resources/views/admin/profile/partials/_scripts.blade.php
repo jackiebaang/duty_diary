ea0e4a04bfbd84e0f676541cb7e647877185817c
Franz <fgelsano@gmail.com>
2023-08-10 19:48:42 +0800

    Keypress event

--- resources/views/admin/profile/partials/_scripts.blade.php
+++ resources/views/admin/profile/partials/_scripts.blade.php
@@ -157,4 +157,43 @@
             }
         });
     })
+
+    $('#name-input').on('keypress',function(event){
+        if(event.keyCode === 13){
+            var form = $('#update-profile-name-form');
+
+            var formData = new FormData(form[0]);
+
+            $.ajax({
+                url: form.attr('action'),
+                type: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function(response) {
+                    Swal.fire({
+                        title: 'Success',
+                        text: response.successMessage,
+                        icon: 'success',
+                        showCancelButton: false,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Okay'
+                    })
+                    .then((result) => {
+                        if (result.isConfirmed) {
+                            location.reload();
+                        }
+                    })
+                },
+                error: function(xhr, status, error) {
+                    console.error('Request failed with status: ' + status);
+                }
+            });
+        }
+    })
 </script>
