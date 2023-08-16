6e7a8face24af4dc83de9694b71f781862bd8040
Franz <fgelsano@gmail.com>
2023-08-16 11:56:32 +0800

    Set isPassChanged column as nullable

--- database/migrations/2023_08_15_075106_add_is_pass_changed_to_users_table.php
+++ database/migrations/2023_08_15_075106_add_is_pass_changed_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsPassChangedToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('isPassChanged')->after('password');
+            $table->integer('isPassChanged')->after('password')->nullable();
         });
     }
 
