510600e57226af71f9fef72bf17bb18e5e360613
Franz <fgelsano@gmail.com>
2023-08-24 03:19:47 +0800

    Updated the password confirm page

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Confirm Password') }}</div>
@@ -43,6 +43,63 @@
                     </form>
                 </div>
             </div>
+        </div>
+    </div> --}}
+
+    <div class="login-box row d-flex justify-content-center align-items-center" style="height: 100vh">
+        <div class="col-md-8 px-0 col-12 justify-content-center align-content-center card card-outline card-primary">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <img src="{{ asset('assets/images/cdl-logo.png') }}" alt="CDL Logo" style="width: 30%" class="cdl-logo">
+                        <img src="{{ asset('assets/icons/diary-icon.ico') }}" alt="Diary Logo" style="width: 8%">
+                    </div>
+                    <div class="col-12 col-md-6 text-right">
+                        <a href="#" class="h3"><b>CDL </b>Duty Diary</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                
+                {{ __('Please confirm your password before continuing.') }}
+
+                <form method="POST" action="{{ route('password.confirm') }}">
+                    @csrf
+
+                    <div class="row mb-3">
+
+                        <div class="col-12">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                    </div>
+
+                    <div class="row mb-0">
+                        <div class="col-md-6 offset-md-3">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                {{ __('Reset Password') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- /.login-card-body -->
         </div>
     </div>
 </div>
