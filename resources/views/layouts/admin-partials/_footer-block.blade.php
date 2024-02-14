f2390321fcf2ffbd0f7f8d4fa168b166b9d47485
Franz <fgelsano@gmail.com>
2024-02-14 19:51:06 +0800

    Updated footer copyright

--- resources/views/layouts/admin-partials/_footer-block.blade.php
+++ resources/views/layouts/admin-partials/_footer-block.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white py-2 mt-2">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
+            <span>Copyright &copy; CreativeDevLabs 2023 [Updated: 2/14/24]</span>
         </div>
     </div>
 </footer>
